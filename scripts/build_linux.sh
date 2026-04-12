9b6a76546bbdd1498fdd151b357b57acc0328d3d
Moulberry <james.jenour@protonmail.com>
2026-04-12 15:07:51 +0800

    Add libfontconfig-dev to fix linux build runner

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -9,7 +9,7 @@ fi
 version=${1#v}
 export PANDORA_RELEASE_VERSION=$version
 
-sudo apt-get update --yes && sudo apt-get install --yes libssl-dev libdbus-1-dev libx11-xcb1 libxkbcommon-x11-dev pkg-config libseccomp-dev
+sudo apt-get update --yes && sudo apt-get install --yes libssl-dev libdbus-1-dev libx11-xcb1 libxkbcommon-x11-dev pkg-config libseccomp-dev libfontconfig-dev
 cargo build --release --target x86_64-unknown-linux-gnu
 strip target/x86_64-unknown-linux-gnu/release/pandora_launcher
 mkdir -p dist
